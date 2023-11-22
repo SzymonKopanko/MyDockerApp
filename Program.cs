8951cb65b7dc9321d5a31cc76d40ef9010ba45f2
Szymon Kopańko <01169576@pw.edu.pl>
2023-11-22 13:59:02 +0000

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyDockerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
